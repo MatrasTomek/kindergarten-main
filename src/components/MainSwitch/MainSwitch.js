8ec7d785c90379a71cff13da513c7ee1c7b2d843
DawidWebb <tomasz.matras@developerweb.pl>
2022-09-03 09:07:49 +0200

    Add Patroness component

--- src/components/MainSwitch/MainSwitch.js
+++ src/components/MainSwitch/MainSwitch.js
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { AboutUs } from "../../components";
+import { AboutUs, Patroness } from "../../components";
 
 const MainSwitch = () => {
 	return (
@@ -7,6 +7,7 @@ const MainSwitch = () => {
 			<Routes>
 				{/* <Route exact path="/" element={<StartViev />} /> */}
 				<Route exact path="/onas" element={<AboutUs />} />
+				<Route exact path="/patronka" element={<Patroness />} />
 				{/* <Route path="*" element={<Navigate to="/" />} /> */}
 			</Routes>
 		</>
--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as MainSwitch } from "./MainSwitch";
 export { default as Menu } from "./Menu";
+export { default as Patroness } from "./Patroness";
 export { default as Spinner } from "./Spinner";
 export { default as TopArrow } from "./TopArrow";
