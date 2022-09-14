73ab16c3470385613df4cea07df98af948bc33d7
DawidWebb <tomasz.matras@developerweb.pl>
2022-09-14 12:41:05 +0200

    Fix table viev in Cadre component

--- src/components/Cadre/Cadre.js
+++ src/components/Cadre/Cadre.js
@@ -15,50 +15,52 @@ const Cadre = () => {
 				</div>
 				<div className={styles.table}>
 					<table>
-						<tr>
-							<th>Dyrektor przedszkola</th>
-							<td>mgr Ewa Tomana</td>
-						</tr>
-						<tr>
-							<th>Kadra pedagogiczna</th>
-							<td>mgr Beata Wolny</td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>mgr Natalia Nabożny</td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>mgr Justyna Fitrzyk</td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>mgr Krystyna Chmielarz</td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>mgr Klaudia Węgrzyn</td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>mgr Anna Chmielarz</td>
-						</tr>
-						<tr>
-							<th>Katecheta</th>
-							<td>ks. mgr Adam Malczyński</td>
-						</tr>
-						<tr>
-							<th>J. angielski</th>
-							<td>mgr Anna Kucia</td>
-						</tr>
-						<tr>
-							<th>Logopeda</th>
-							<td>mgr Klaudia Węgrzyn</td>
-						</tr>
-						<tr>
-							<th>Rytmika</th>
-							<td>mgr Andrzej Gicala</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th>Dyrektor przedszkola</th>
+								<td>mgr Ewa Tomana</td>
+							</tr>
+							<tr>
+								<th>Kadra pedagogiczna</th>
+								<td>mgr Beata Wolny</td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>mgr Natalia Nabożny</td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>mgr Justyna Fitrzyk</td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>mgr Krystyna Chmielarz</td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>mgr Klaudia Węgrzyn</td>
+							</tr>
+							<tr>
+								<th></th>
+								<td>mgr Anna Chmielarz</td>
+							</tr>
+							<tr>
+								<th>Katecheta</th>
+								<td>ks. mgr Adam Malczyński</td>
+							</tr>
+							<tr>
+								<th>J. angielski</th>
+								<td>mgr Anna Kucia</td>
+							</tr>
+							<tr>
+								<th>Logopeda</th>
+								<td>mgr Klaudia Węgrzyn</td>
+							</tr>
+							<tr>
+								<th>Rytmika</th>
+								<td>mgr Andrzej Gicala</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 				<div className={styles.pictureItem1}>
@@ -66,51 +68,52 @@ const Cadre = () => {
 				</div>
 				<div className={styles.table}>
 					<table>
-						<tr>
-							<th>Pomoc do dzieci:</th>
-							<td></td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>pani Monika Latocha </td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>pani Mariola Czekała</td>
-						</tr>
-						<tr>
-							<th>O sprawy finansowe dba</th>
-							<td> pani Krystyna Kubala</td>
-						</tr>
-						<tr>
-							<th>Smaczne posiłki przygotowują:</th>
-							<td></td>
-						</tr>
-						<td></td>
-						<tr>
-							<th>Szef kuchni</th>
-							<td>pani Joanna Niedźwiecka</td>
-						</tr>
-						<tr>
-							<th>pomoc w kuchni</th>
-							<td> pani Bogumiła Biga</td>
-						</tr>
-						<tr>
-							<th>O piękny ogród dba</th>
-							<td>pani Magdalena Pasula</td>
-						</tr>
-						<tr>
-							<th>O czystość i estetykę przedszkola dba</th>
-							<td></td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>pani Katarzyna Orzeł – Rudnik </td>
-						</tr>
-						<tr>
-							<th></th>
-							<td>pani Paulina Biedak</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th>Pomoc do dzieci:</th>
+								<td>{""}</td>
+							</tr>
+							<tr>
+								<th>{""}</th>
+								<td>pani Monika Latocha </td>
+							</tr>
+							<tr>
+								<th>{""}</th>
+								<td>pani Mariola Czekała</td>
+							</tr>
+							<tr>
+								<th>O sprawy finansowe dba</th>
+								<td> pani Krystyna Kubala</td>
+							</tr>
+							<tr>
+								<th>Smaczne posiłki przygotowują:</th>
+								<td>{""}</td>
+							</tr>
+							<tr>
+								<th>Szef kuchni</th>
+								<td>pani Joanna Niedźwiecka</td>
+							</tr>
+							<tr>
+								<th>pomoc w kuchni</th>
+								<td> pani Bogumiła Biga</td>
+							</tr>
+							<tr>
+								<th>O piękny ogród dba</th>
+								<td>pani Magdalena Pasula</td>
+							</tr>
+							<tr>
+								<th>O czystość i estetykę przedszkola dba</th>
+								<td>{""}</td>
+							</tr>
+							<tr>
+								<th>{""}</th>
+								<td>pani Katarzyna Orzeł – Rudnik </td>
+							</tr>
+							<tr>
+								<th>{""}</th>
+								<td>pani Paulina Biedak</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 				<div className={styles.pictureItem1}>
@@ -121,250 +124,252 @@ const Cadre = () => {
 						<span>Zatrudnieni w Parafialnym Przedszkolu w Mokrzyskach</span>
 					</p>
 					<table>
-						<tr>
-							<td>
-								<span>Ks. Marian Wal</span>
-							</td>
-							<td>1990 – 2007</td>
-						</tr>
-						<tr>
-							<td>
-								<span>Ks. mgr Jan Panek</span>
-							</td>
-							<td>2007 – 2021</td>
-						</tr>
-						<tr>
-							<td>p. Krystyna Biedak</td>
-							<td>1990 – 2013</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Stanisława Wojtyczka</span>
-								</p>
-							</td>
-							<td>1990 – 1995</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Teresa Słonina</span>
-								</p>
-							</td>
-							<td> 1990 – 1991</td>
-						</tr>
-						<tr>
-							<td>p. Bogumiła Kośmider</td>
-							<td>1990 – 1992, 1994 – 1996</td>
-						</tr>
-						<tr>
-							<td>p. Krystyna Kubala</td>
-							<td>1990 –</td>
-						</tr>
-						<tr>
-							<td>p. Bogumiła Cyran</td>
-							<td>1990 – 2007</td>
-						</tr>
-						<tr>
-							<td>p. Krystyna Lechowicz</td>
-							<td>1990 – 1993</td>
-						</tr>
-						<tr>
-							<td>p. Anna Borowiec</td>
-							<td>1990 – 1991</td>
-						</tr>
-						<tr>
-							<td>p. Halina Palej</td>
-							<td>1990 – 2016</td>
-						</tr>
-						<tr>
-							<td>p. Norbert Kubiczek</td>
-							<td>1990 – 1991</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Małgorzata Kapłon</span>
-								</p>
-							</td>
-							<td>1991 – 1994</td>
-						</tr>
-						<tr>
-							<td>p. Marta Godzik</td>
-							<td>1991 – 1992</td>
-						</tr>
-						<tr>
-							<td>p. Ewa Tomana</td>
-							<td>1992 -</td>
-						</tr>
-						<tr>
-							<td>p. Grażyna Szylar- Toboła</td>
-							<td> 1992 – 1994</td>
-						</tr>
-						<tr>
-							<td>p. Cecylia Ręczak</td>
-							<td>1992 – 2006</td>
-						</tr>
-						<tr>
-							<td>p. Agata Strojny</td>
-							<td>1992 – 1993</td>
-						</tr>
-						<tr>
-							<td>p. Teresa Zachara</td>
-							<td> 1992 – 1993</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Stanisława Szczerba</span>
-								</p>
-							</td>
-							<td>1994 – 1995</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Danuta Gromada</span>
-								</p>
-							</td>
-							<td>1995 – 1996</td>
-						</tr>
-						<tr>
-							<td>p. Maria Turlej</td>
-							<td> 1995 –</td>
-						</tr>
-						<tr>
-							<td>p. Cecylia Kuciel</td>
-							<td>1995 – 2015</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Agata Szczygielska</span>
-								</p>
-							</td>
-							<td>1997- 2001</td>
-						</tr>
-						<tr>
-							<td>p. Agata Jabłońska</td>
-							<td> 1997 – 1999</td>
-						</tr>
-						<tr>
-							<td>p. Jolanta Król</td>
-							<td>1997 – 1998</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Renata Szeląg</span>
-								</p>
-							</td>
-							<td> 2001 – 2007</td>
-						</tr>
-						<tr>
-							<td>p. Józefa Sury</td>
-							<td> 2003 – 2004</td>
-						</tr>
-						<tr>
-							<td>p. Beata Wolny</td>
-							<td>2006 -</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Maria Kiłyk</span>
-								</p>
-							</td>
-							<td>2007 – 2008</td>
-						</tr>
-						<tr>
-							<td>p. Joanna Niedojadło</td>
-							<td> 2007 – 2008</td>
-						</tr>
-						<tr>
-							<td>p. Anna Palej</td>
-							<td>2007 – 2009</td>
-						</tr>
-						<tr>
-							<td>
-								<p>
-									<span>s. Agnieszka Boratyn</span>
-								</p>
-							</td>
-							<td>2008 – 2009</td>
-						</tr>
-						<tr>
-							<td>p. Bogusława Biga</td>
-							<td>2008 -</td>
-						</tr>
-						<tr>
-							<td>p. Natalia Nabożny</td>
-							<td>2009 -</td>
-						</tr>
-						<tr>
-							<td>p. Krystyna Chmielarz</td>
-							<td>2010 -</td>
-						</tr>
-						<tr>
-							<td>p. Katarzyna Mazur</td>
-							<td>2011 – 2012</td>
-						</tr>
-						<tr>
-							<td>p. Joanna Niedźwiecka</td>
-							<td>2011 -</td>
-						</tr>
-						<tr>
-							<td>p. Magdalena Pasula</td>
-							<td>2011 -</td>
-						</tr>
-						<tr>
-							<td>p. Justyna Fitrzyk</td>
-							<td>2012 -</td>
-						</tr>
-						<tr>
-							<td>p. Dorota Kazek</td>
-							<td>2012 – 2018</td>
-						</tr>
-						<tr>
-							<td>p. Monika Latocha</td>
-							<td>2014 -</td>
-						</tr>
-						<tr>
-							<td>p. Klaudia Węgrzyn</td>
-							<td>2015 -</td>
-						</tr>
-						<tr>
-							<td>p. Mariola Czekała</td>
-							<td>2016 -</td>
-						</tr>
-						<tr>
-							<td>p. Joanna Pasula</td>
-							<td>2017 – 2019</td>
-						</tr>
-						<tr>
-							<td>p. Katarzyna Orzeł- Rudnik</td>
-							<td>2019 -</td>
-						</tr>
-						<tr>
-							<td>p. Katarzyna Derela- Gawenda</td>
-							<td>2019 – 2021</td>
-						</tr>
-						<tr>
-							<td>p. Andrzej Gicala</td>
-							<td>2019 -</td>
-						</tr>
-						<tr>
-							<td>p. Anna Chmielarz</td>
-							<td>2019 -</td>
-						</tr>
-						<tr>
-							<td>p. Paulina Biedak</td>
-							<td>2020 -</td>
-						</tr>
-						<tr>
-							<td>p. Anna Kucia</td>
-							<td>2021 -</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th>
+									<span>Ks. Marian Wal</span>
+								</th>
+								<td>1990 – 2007</td>
+							</tr>
+							<tr>
+								<th>
+									<span>Ks. mgr Jan Panek</span>
+								</th>
+								<td>2007 – 2021</td>
+							</tr>
+							<tr>
+								<th>p. Krystyna Biedak</th>
+								<td>1990 – 2013</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Stanisława Wojtyczka</span>
+									</p>
+								</th>
+								<td>1990 – 1995</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Teresa Słonina</span>
+									</p>
+								</th>
+								<td> 1990 – 1991</td>
+							</tr>
+							<tr>
+								<th>p. Bogumiła Kośmider</th>
+								<td>1990 – 1992, 1994 – 1996</td>
+							</tr>
+							<tr>
+								<th>p. Krystyna Kubala</th>
+								<td>1990 –</td>
+							</tr>
+							<tr>
+								<th>p. Bogumiła Cyran</th>
+								<td>1990 – 2007</td>
+							</tr>
+							<tr>
+								<th>p. Krystyna Lechowicz</th>
+								<td>1990 – 1993</td>
+							</tr>
+							<tr>
+								<th>p. Anna Borowiec</th>
+								<td>1990 – 1991</td>
+							</tr>
+							<tr>
+								<th>p. Halina Palej</th>
+								<td>1990 – 2016</td>
+							</tr>
+							<tr>
+								<th>p. Norbert Kubiczek</th>
+								<td>1990 – 1991</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Małgorzata Kapłon</span>
+									</p>
+								</th>
+								<td>1991 – 1994</td>
+							</tr>
+							<tr>
+								<th>p. Marta Godzik</th>
+								<td>1991 – 1992</td>
+							</tr>
+							<tr>
+								<th>p. Ewa Tomana</th>
+								<td>1992 -</td>
+							</tr>
+							<tr>
+								<th>p. Grażyna Szylar- Toboła</th>
+								<td> 1992 – 1994</td>
+							</tr>
+							<tr>
+								<th>p. Cecylia Ręczak</th>
+								<td>1992 – 2006</td>
+							</tr>
+							<tr>
+								<th>p. Agata Strojny</th>
+								<td>1992 – 1993</td>
+							</tr>
+							<tr>
+								<th>p. Teresa Zachara</th>
+								<td> 1992 – 1993</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Stanisława Szczerba</span>
+									</p>
+								</th>
+								<td>1994 – 1995</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Danuta Gromada</span>
+									</p>
+								</th>
+								<td>1995 – 1996</td>
+							</tr>
+							<tr>
+								<th>p. Maria Turlej</th>
+								<td> 1995 –</td>
+							</tr>
+							<tr>
+								<th>p. Cecylia Kuciel</th>
+								<td>1995 – 2015</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Agata Szczygielska</span>
+									</p>
+								</th>
+								<td>1997- 2001</td>
+							</tr>
+							<tr>
+								<th>p. Agata Jabłońska</th>
+								<td> 1997 – 1999</td>
+							</tr>
+							<tr>
+								<th>p. Jolanta Król</th>
+								<td>1997 – 1998</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Renata Szeląg</span>
+									</p>
+								</th>
+								<td> 2001 – 2007</td>
+							</tr>
+							<tr>
+								<th>p. Józefa Sury</th>
+								<td> 2003 – 2004</td>
+							</tr>
+							<tr>
+								<th>p. Beata Wolny</th>
+								<td>2006 -</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Maria Kiłyk</span>
+									</p>
+								</th>
+								<td>2007 – 2008</td>
+							</tr>
+							<tr>
+								<th>p. Joanna Niedojadło</th>
+								<td> 2007 – 2008</td>
+							</tr>
+							<tr>
+								<th>p. Anna Palej</th>
+								<td>2007 – 2009</td>
+							</tr>
+							<tr>
+								<th>
+									<p>
+										<span>s. Agnieszka Boratyn</span>
+									</p>
+								</th>
+								<td>2008 – 2009</td>
+							</tr>
+							<tr>
+								<th>p. Bogusława Biga</th>
+								<td>2008 -</td>
+							</tr>
+							<tr>
+								<th>p. Natalia Nabożny</th>
+								<td>2009 -</td>
+							</tr>
+							<tr>
+								<th>p. Krystyna Chmielarz</th>
+								<td>2010 -</td>
+							</tr>
+							<tr>
+								<th>p. Katarzyna Mazur</th>
+								<td>2011 – 2012</td>
+							</tr>
+							<tr>
+								<th>p. Joanna Niedźwiecka</th>
+								<td>2011 -</td>
+							</tr>
+							<tr>
+								<th>p. Magdalena Pasula</th>
+								<td>2011 -</td>
+							</tr>
+							<tr>
+								<th>p. Justyna Fitrzyk</th>
+								<td>2012 -</td>
+							</tr>
+							<tr>
+								<th>p. Dorota Kazek</th>
+								<td>2012 – 2018</td>
+							</tr>
+							<tr>
+								<th>p. Monika Latocha</th>
+								<td>2014 -</td>
+							</tr>
+							<tr>
+								<th>p. Klaudia Węgrzyn</th>
+								<td>2015 -</td>
+							</tr>
+							<tr>
+								<th>p. Mariola Czekała</th>
+								<td>2016 -</td>
+							</tr>
+							<tr>
+								<th>p. Joanna Pasula</th>
+								<td>2017 – 2019</td>
+							</tr>
+							<tr>
+								<th>p. Katarzyna Orzeł- Rudnik</th>
+								<td>2019 -</td>
+							</tr>
+							<tr>
+								<th>p. Katarzyna Derela- Gawenda</th>
+								<td>2019 – 2021</td>
+							</tr>
+							<tr>
+								<th>p. Andrzej Gicala</th>
+								<td>2019 -</td>
+							</tr>
+							<tr>
+								<th>p. Anna Chmielarz</th>
+								<td>2019 -</td>
+							</tr>
+							<tr>
+								<th>p. Paulina Biedak</th>
+								<td>2020 -</td>
+							</tr>
+							<tr>
+								<th>p. Anna Kucia</th>
+								<td>2021 -</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 				<div className={styles.table}>
@@ -372,22 +377,25 @@ const Cadre = () => {
 						<span>Dyrektorzy</span>
 					</p>
 					<table>
-						<tr>
-							<td>p. Krystyna Biedak</td>
-							<td>1990 – 1991</td>
-						</tr>
-						<tr>
-							<td>s. Małgorzata Kapłon</td>
-							<td>1991 – 1994</td>
-						</tr>
-						<tr>
-							<td>s. Stanisława Szczerba</td>
-							<td>1994 – 1995</td>
-						</tr>
-						<tr>
-							<td>p. Ewa Tomana</td>
-							<td>1995 –</td>
-						</tr>
+						<tbody>
+							<tr>
+								<th>p. Krystyna Biedak</th>
+								<td>1990 – 1991</td>
+							</tr>
+							<tr></tr>
+							<tr>
+								<th>s. Małgorzata Kapłon</th>
+								<td>1991 – 1994</td>
+							</tr>
+							<tr>
+								<th>s. Stanisława Szczerba</th>
+								<td>1994 – 1995</td>
+							</tr>
+							<tr>
+								<th>p. Ewa Tomana</th>
+								<td>1995 –</td>
+							</tr>
+						</tbody>
 					</table>
 				</div>
 			</div>
