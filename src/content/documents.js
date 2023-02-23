387d7122dc9acc5bb3343b63ac5e5e615b7eea9a
DawidWebb <tomasz.matras@developerweb.pl>
2023-02-23 22:33:36 +0100

    Update documents

--- src/content/documents.js
+++ src/content/documents.js
@@ -1,75 +1,70 @@
 export const DOCS_ABOUT_KINDERGARTEN = [
-	{
-		id: "satute",
-		name: "Statut [PDF]",
-		path: "/akt.pdf",
-	},
-	{
-		id: "base",
-		name: "Podstawa programowa [PDF]",
-		path: "PODSTAWA_2016.pdf",
-	},
-	{
-		id: "concept",
-		name: "Koncepcja pracy przedszkola [PDF]",
-		path: "KONCEPCJA.pdf",
-	},
-	{
-		id: "procedures",
-		name: "Procedury [PDF]",
-		path: "PROCEDURY.pdf",
-	},
-	{
-		id: "procedures-new",
-		name: "Procedury [PDF] ważne od 01.06.2019",
-		path: "PROCEDURY_1.pdf",
-	},
-	{
-		id: "innovations-tales",
-		name: "Innowacje – „Bajki uczą życia” [PDF]",
-		path: "Innowacja.pdf",
-	},
-	{
-		id: "innovations-math",
-		name: "Innowacje – „Mali Matematycy” [PDF]",
-		path: "Innowacjapedagogiczna.pdf",
-	},
-	{
-		id: "innovations-open",
-		name: "Innowacje – „Otwarte drzwi – przyjaciel w innej grupie” [PDF]",
-		path: "Innowacjapedagogicznam",
-	},
-	{
-		id: "project-edu",
-		name: "Projekty edukacyjny [PDF]",
-		path: "PROJEKTEDUKACYJNYv1.pdf",
-	},
-	{
-		id: "project-bear",
-		name: "Projekt – „Miś w świecie literatury” [PDF]",
-		path: "MALYMIS1.pdf",
-	},
-	{
-		id: "schedule",
-		name: "Ramowy rozkład dnia [PDF]",
-		path: "RAMOWY_ROZKLAD.pdf",
-	},
+  {
+    id: "satute",
+    name: "Statut [PDF]",
+    path: "/akt.pdf",
+  },
+  {
+    id: "base",
+    name: "Podstawa programowa [PDF]",
+    path: "PODSTAWA_2016.pdf",
+  },
+  {
+    id: "concept",
+    name: "Koncepcja pracy przedszkola [PDF]",
+    path: "KONCEPCJA.pdf",
+  },
+  {
+    id: "procedures",
+    name: "Procedury [PDF]",
+    path: "PROCEDURY.pdf",
+  },
+  {
+    id: "procedures-new",
+    name: "Procedury [PDF] ważne od 01.06.2019",
+    path: "PROCEDURY_1.pdf",
+  },
+  {
+    id: "innovations-tales",
+    name: "Innowacje – „Bajki uczą życia” [PDF]",
+    path: "Innowacja.pdf",
+  },
+  {
+    id: "innovations-math",
+    name: "Innowacje – „Mali Matematycy” [PDF]",
+    path: "Innowacjapedagogiczna.pdf",
+  },
+  {
+    id: "innovations-open",
+    name: "Innowacje – „Otwarte drzwi – przyjaciel w innej grupie” [PDF]",
+    path: "Innowacjapedagogicznam",
+  },
+  {
+    id: "project-edu",
+    name: "Projekty edukacyjny [PDF]",
+    path: "PROJEKTEDUKACYJNYv1.pdf",
+  },
+  {
+    id: "project-bear",
+    name: "Projekt – „Miś w świecie literatury” [PDF]",
+    path: "MALYMIS1.pdf",
+  },
+  {
+    id: "schedule",
+    name: "Ramowy rozkład dnia [PDF]",
+    path: "RAMOWY_ROZKLAD.pdf",
+  },
 ];
 
 export const DOCS_ABOUT_RECRUITMENT = [
-	{
-		id: "continue-card",
-		name: "Karta o kontynuacji [PDF]",
-		path: "KARTA-O-KONTYNUACJI1.pdf",
-	},
-	{
-		id: "registration-card",
-		name: "Karta zgłoszenia dziecka [PDF]",
-		path: "KARTA-DO-ZAPISU-DZIECKA-DO-PRZEDSZKOLA.pdf",
-	},
-	{
-		id: "criteria-card",
-		name: "Karta kryteriów rekrutacji [PDF]",
-		path: "KARTA-KRYTERIÓW-REKRUTACJI.pdf",
-	},
+  {
+    id: "continue-card",
+    name: "Deklaracja o kontynuacji [PDF]",
+    path: "KARTA-O-KONTYNUACJI1.pdf",
+  },
+  {
+    id: "registration-card",
+    name: "Karta zgłoszenia rok 2023-2024 [PDF]",
+    path: "KARTA-DO-ZAPISU-DZIECKA-DO-PRZEDSZKOLA.pdf",
+  },
 ];
