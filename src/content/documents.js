975ae3a26b97bb0775d575fb86ab31a5b7d3736f
Tomek Matras <matras.tomasz@outlook.com>
2026-01-27 19:16:10 +0100

    Refactor document structure for kindergarten and recruitment sections

--- src/content/documents.js
+++ src/content/documents.js
@@ -1,65 +1,65 @@
 export const DOCS_ABOUT_KINDERGARTEN = [
-  {
-    id: "satute",
-    name: "Statut [PDF]",
-    path: "/akt.pdf",
-  },
-  {
-    id: "base",
-    name: "Podstawa programowa [PDF]",
-    path: "PODSTAWA_2016.pdf",
-  },
-  {
-    id: "concept",
-    name: "Koncepcja pracy przedszkola [PDF]",
-    path: "KONCEPCJA.pdf",
-  },
-  {
-    id: "procedures-new",
-    name: "Procedury obowiązujące w przedszkolu",
-    path: "PROCEDURY_1.pdf",
-  },
-  {
-    id: "innovations-tales",
-    name: "Innowacje – „Bajki uczą życia” [PDF]",
-    path: "Innowacja.pdf",
-  },
-  {
-    id: "innovations-math",
-    name: "Innowacje – „Mali Matematycy” [PDF]",
-    path: "Innowacjapedagogiczna.pdf",
-  },
-  {
-    id: "innovations-open",
-    name: "Innowacje – „Otwarte drzwi – przyjaciel w innej grupie” [PDF]",
-    path: "Innowacjapedagogicznam",
-  },
-  {
-    id: "project-edu",
-    name: "Projekty edukacyjny [PDF]",
-    path: "PROJEKTEDUKACYJNYv1.pdf",
-  },
-  {
-    id: "project-bear",
-    name: "Projekt – „Miś w świecie literatury” [PDF]",
-    path: "MALYMIS1.pdf",
-  },
+	{
+		id: 'satute',
+		name: 'Statut [PDF]',
+		path: '/akt.pdf',
+	},
+	{
+		id: 'base',
+		name: 'Podstawa programowa [PDF]',
+		path: 'PODSTAWA_2016.pdf',
+	},
+	{
+		id: 'concept',
+		name: 'Koncepcja pracy przedszkola [PDF]',
+		path: 'KONCEPCJA.pdf',
+	},
+	{
+		id: 'procedures-new',
+		name: 'Procedury obowiązujące w przedszkolu',
+		path: 'PROCEDURY_1.pdf',
+	},
+	{
+		id: 'innovations-tales',
+		name: 'Innowacje – „Bajki uczą życia” [PDF]',
+		path: 'Innowacja.pdf',
+	},
+	{
+		id: 'innovations-math',
+		name: 'Innowacje – „Mali Matematycy” [PDF]',
+		path: 'Innowacjapedagogiczna.pdf',
+	},
+	{
+		id: 'innovations-open',
+		name: 'Innowacje – „Otwarte drzwi – przyjaciel w innej grupie” [PDF]',
+		path: 'Innowacjapedagogicznam',
+	},
+	{
+		id: 'project-edu',
+		name: 'Projekty edukacyjny [PDF]',
+		path: 'PROJEKTEDUKACYJNYv1.pdf',
+	},
+	{
+		id: 'project-bear',
+		name: 'Projekt – „Miś w świecie literatury” [PDF]',
+		path: 'MALYMIS1.pdf',
+	},
 ];
 
 export const DOCS_ABOUT_RECRUITMENT = [
-  {
-    id: "continue-card",
-    name: "Deklaracja o kontynuacji [PDF]",
-    path: "KARTA-O-KONTYNUACJI1.pdf",
-  },
-  {
-    id: "registration-card",
-    name: "Karta zgłoszenia rok 2024-2025 [PDF]",
-    path: "KARTA-DO-ZAPISU-DZIECKA-DO-PRZEDSZKOLA.pdf",
-  },
-  {
-    id: "schedule-card",
-    name: "Harmonogram czynności w postępowaniu rekrutacyjnym [PDF]",
-    path: "Harmonogram_czynności_w_postępowaniu_rekrutacyjnym_oraz_postępowaniu_uzupełniającym.pdf",
-  },
+	{
+		id: 'continue-card',
+		name: 'Deklaracja o kontynuacji [PDF]',
+		path: 'DEKLARACJA_o_kontynuowaniu_wychowania_przedszkolnego.pdf',
+	},
+	{
+		id: 'registration-card',
+		name: 'Karta zgłoszenia rok 2026-2027 [PDF]',
+		path: 'KARTA-DO-ZAPISU-DZIECKA-DO-PRZEDSZKOLA_26_27.pdf',
+	},
+	{
+		id: 'schedule-card',
+		name: 'Harmonogram czynności w postępowaniu rekrutacyjnym [PDF]',
+		path: 'HARMONOGRAM-REKRUTACJI-DO-PUBLICZNEGO-PRZEDSZKOLA.pdf',
+	},
 ];
