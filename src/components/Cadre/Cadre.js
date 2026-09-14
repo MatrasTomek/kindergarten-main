9f4a4bdbc42e0e866eff58ae8317b0c820b8e5c7
Tomasz Matras <tomasz.matras@outlook.com>
2026-09-14 09:55:35 +0200

    Update Cadre component and styles: Revise staff details, enhance layout, and improve styling for better readability

--- src/components/Cadre/Cadre.js
+++ src/components/Cadre/Cadre.js
@@ -8,7 +8,10 @@ const Cadre = () => {
 			<div className={styles.inside}>
 				<h2>Nasza kadra</h2>
 				<div className={styles.title}>
-					<p>Organ prowadzący przedszkole: ks. Tomasz Szewczyk</p>
+					<p>
+						Organ prowadzący przedszkole: Parafia Najświętszego Serca Jezusowego w Mokrzyskach osoba reprezentująca{' '}
+						<span>ks Tomasz Szewczyk</span>
+					</p>
 				</div>
 				<div className={styles.table}>
 					<table>
@@ -17,98 +20,57 @@ const Cadre = () => {
 								<th>Dyrektor Przedszkola:</th>
 								<td>Katarzyna Brożek</td>
 							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className={styles.pictureItem1}>
+					{/* <img src="/images/kadra/grono.jpg" alt="kadra przedszkola" /> */}
+					<p>NAUCZYCIELE PRACUJĄCY W PRZEDSZKOLU:</p>
+				</div>
+				<div className={styles.table}>
+					<table>
+						<tbody>
 							<tr>
-								<th>Vice dyrektor:</th>
-								<td>Ewa Tomana</td>
+								<th>Grupa Tygryski:</th>
+								<td>Pani Klaudia</td>
+							</tr>
+							<tr>
+								<th>Grupa Kangurki:</th>
+								<td>Pani Anna</td>
+							</tr>
+							<tr>
+								<th>Grupa Pingwinki:</th>
+								<td>Pani Luiza i Pani Monika</td>
+							</tr>
+							<tr>
+								<th>Grupa Ananaski:</th>
+								<td>Pani Angelika</td>
+							</tr>
+							<tr>
+								<th>Grupa Ananaski i Tygryski:</th>
+								<td>Pani Ewa</td>
 							</tr>
 						</tbody>
 					</table>
 				</div>
 				<div className={styles.pictureItem1}>
 					{/* <img src="/images/kadra/grono.jpg" alt="kadra przedszkola" /> */}
-					<p>Nauczyciele:</p>
+					<p>PRZEDSZKOLE ZATRUDNIA SPECJALISTÓW:</p>
 				</div>
 				<div className={styles.table}>
 					<table>
 						<tbody>
 							<tr>
-								<th>Oddział 1:</th>
-								<td>Pani Klaudia Węgrzyn</td>
+								<th>J. Angielski:</th>
+								<td>Pani Kasia</td>
 							</tr>
-							<tr>
-								<th>Oddział 2:</th>
-								<td>Pani Natalia Nabożny</td>
-							</tr>
-							<tr>
-								<th>Oddział 3:</th>
-								<td>Pani Paulina Jarosz, Pani Luiza Biel</td>
-							</tr>
-							<tr>
-								<th>Oddział 4:</th>
-								<td>Pani Anna Chmielarz</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div className={styles.pictureItem1}>{/* <img src="/images/kadra/grono.jpg" alt="kadra przedszkola" /> */}</div>
-				<div className={styles.table}>
-					<table>
-						<tbody>
 							<tr>
 								<th>Psycholog:</th>
-								<td>Pani Weronika Wieczorek</td>
-							</tr>
-							<tr>
-								<th>Pedagog:</th>
-								<td>Pani Luiza Bie</td>
+								<td>Pani Weronika</td>
 							</tr>
 							<tr>
 								<th>Logopeda:</th>
-								<td>Pani Klaudia Węgrzyn</td>
-							</tr>
-							<tr>
-								<th>J. Angielski:</th>
-								<td>Pani Katarzyna Gwóźdź</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div className={styles.pictureItem1}>
-					{/* <img src="/images/kadra/grono.jpg" alt="kadra przedszkola" /> */}
-					<p>Pracownicy obsługi:</p>
-				</div>
-				<div className={styles.table}>
-					<table>
-						<tbody>
-							<tr>
-								<td>Krystyna Kubala - księgowa</td>
-							</tr>
-							<tr>
-								<td>Małgorzata Bober - intendent</td>
-							</tr>
-							<tr>
-								<td>Monika Latocha – pomoc do dzieci</td>
-							</tr>
-							<tr>
-								<td>Katarzyna Orzeł Rudnik – pomoc do dzieci</td>
-							</tr>
-							<tr>
-								<td>Bogusława Biga – pomoc szefa kuchni</td>
-							</tr>
-							<tr>
-								<td>Mariola Czekała– pomoc do dzieci</td>
-							</tr>
-							<tr>
-								<td>Joanna Niedźwiedzka – szef kuchni</td>
-							</tr>
-							<tr>
-								<td>Oleksyk Wojciech - konserwator</td>
-							</tr>
-							<tr>
-								<td>Magdalena Pasula - woźna</td>
-							</tr>
-							<tr>
-								<td>Paulina Biedak - woźna</td>
+								<td>Pani Joasia</td>
 							</tr>
 						</tbody>
 					</table>
