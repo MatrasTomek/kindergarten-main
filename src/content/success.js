b8a75fb61e2ea786766e291faccbfd0ce3772b3c
Tomasz Matras <tomasz.matras@outlook.com>
2026-09-14 10:52:06 +0200

    Remove outdated success items from the success.js file to streamline content

--- src/content/success.js
+++ src/content/success.js
@@ -1,120 +1,20 @@
 export const SUCCESS_ITEMS = [
-  {
-    id: "success12",
-    date: "21.06.2024",
-    title: "Hawajsko-Piracka nocka Przedszkola",
-    content1:
-      "",
-    signature: "",
-    imagePath: "images/noc_2024",
-    imgFolderLength: 13,
-  },
-  {
-    id: "success11",
-    date: "28.06.2024",
-    title: "Poświęcenie figury Najświetszego Serca Pana Jezusa przy budynku Przedszkola, po dokonanej renowacji",
-    content1:
-      "",
-    signature: "",
-    imagePath: "images/figura_posw_2024",
-    imgFolderLength: 10,
-  },
-  {
-    id: "success10",
-    date: "03.04.2023",
-    title: "Konkurs na najpiękniejszy 'Stroik wielkanocny' rozstrzygnięty",
-    content1:
-      " W konkursie wzięło udział 39 dzieci z naszego przedszkola, które z pomocą swoich rodziców i dziadków wykonały piękne i pomysłowe prace. Za niezwykłą kreatywność i serce włożone w wykonanie stroików, wszystkie dzieci zostały wyróżnione otrzymując dyplomy i nagrody rzeczowe. Gratulacje 👏👏👏 Stroiki zostały przekazane na świąteczny kiermasz, który został zorganizowany po wczorajszej mszy świętej pod naszym parafialnym kościołem. Udało się zebrać 1700 zł 🙂🙂🙂 Pieniążki te zostaną przekazane na dofinansowanie pikniku rodzinnego, który odbędzie się w czerwcu 🥰 Składamy ogromne podziękowania rodzicom, dziadkom i wszystkim, którzy zakupili stroiki a dzieciom jeszcze raz gratulujemy 👏",
-    signature: "",
-    imagePath: "images/stolik_wlk_2023",
-    imgFolderLength: 14,
-  },
-  {
-    id: "success9",
-    date: "31.03.2023",
-    title: "Rekolekcje Wielkopostne dla naszych Maluchów",
-    content1:
-      "W dniu 31 marca br., odbyły się Rekolekcje Wielkopostne dla naszych Maluchów z Przedszkola, które prowadził ks. Sylwester Brzeżny.",
-    signature: "",
-    imagePath: "images/rekolekcje_wlp_2023",
-    imgFolderLength: 22,
-  },
-  {
-    id: "success8",
-    date: "21.03.2023",
-    title: "Otwarcie i Poświęcenie nowego urządzenia na placu zabaw",
-    content1:
-      "21 marca 2023 roku w Publicznym Przedszkolu Parafialnym w Mokrzyskach, obyło się uroczyste powitanie wiosny przez dzieci z poszczególnych grup. Przedszkolaki wesołą zabawą pożegnały zimę. Słychać było radosne okrzyki „Witaj Wiosno!”. Natomiast dzień 22 marzec, był dniem dla wszystkich wyjątkowo radosnym, ponieważ Pani Dyrektor i ks. Proboszcz dokonali uroczystego otwarcia i poświęcenia nowego, wymarzonego urządzenia na placu zabaw. Spełniło się marzenie wielu dzieci, nauczycieli i rodziców. Przedszkolaki będą mogły miło i bezpiecznie spędzać czas na świeżym powietrzu. Przez zabawę, nauka stanie się przyjemniejsza. Kolorowy i bezpieczny sprzęt, pozwoli dzieciom urozmaicać czas, a także zaspokoić potrzeby ruchowe. Radość i uśmiech na twarzach przedszkolaków były największym podziękowaniem.",
-    signature: "",
-    imagePath: "images/plac_zabaw_2023",
-    imgFolderLength: 32,
-  },
-  {
-    id: "success7",
-    date: "28.01.2023",
-    title: "XVIII Przegląd Kolęd i Pastorałek O Muszlę św. Jakuba",
-    content1:
-      "Dnia 28.01.2023 r. w kościele pw. NMP Matki Kościoła i św. Jakuba Apostoła w Brzesku odbył się XVIII Przegląd Kolęd i Pastorałek O Muszlę św. Jakuba .Miło nam poinformować,  że najstarsza grupa z naszego przedszkola 'Przyjaciele Jezuska' pod opieką Pana Andrzeja Gicali zdobyły zaszczytne III miejsce. Gratulujemy naszym milusińskim i ich rodzicom oraz życzymy dalszych sukcesów.",
-    signature: "",
-    imagePath: "images/przegladKoled",
-    imgFolderLength: 9,
-  },
-  {
-    id: "success6",
-    date: "21.01.2023",
-    title: "Dzień Babci i Dziadka 2023",
-    content1:
-      "Dzień Babci i Dziadka to wyjątkowa uroczystość. Z tej też okazji 21 stycznia w naszym Kościele Parafialnym w Mokrzyskach została odprawiona Msza święta pod przewodnictwem Ks. Proboszcza, podczas której wspólnie modliliśmy się o zdrowie i błogosławieństwo Boże dla naszych kochanych Dziadków. Po mszy św. Przedszkolaki zaprezentowały krótki program artystyczny, recytując wiersze oraz śpiewając piosenki i najpiękniejsze kolędy i pastorałki. Następnie Ks. Proboszcz wyraził podziękowania wobec małych wykonawców i nauczycieli przygotowujących program muzyczny. Kochanym Babciom i Dziadkom jeszcze raz Dyrekcja i pracownicy przedszkola, na czele z Organem Prowadzącym złożyli życzenia zdrowia, pomyślności i samych pięknych chwil spędzonych ze swoimi wnukami.",
-    signature: "",
-    imagePath: "images/dBabci2023",
-    imgFolderLength: 101,
-  },
-  {
-    id: "success5",
-    date: "18.11.2022",
-    title:
-      "18 XI 2022. Wspomnienie Bł. Karoliny Kózkówny, patronki naszego Przedszkola.",
-    content1: "",
-    signature: "",
-    imagePath: "images/swieto_patronki2022",
-    imgFolderLength: 56,
-  },
-  {
-    id: "success4",
-    date: "05.10.2022",
-    title: "Baśnie Pana Andersena",
-    content1:
-      "W dniu 05.10.2022r w Brzesku, przedszkolaki uczestniczyli w spektaklu Baśnie Pana Andersena.",
-    signature: "",
-    imagePath: "images/basnieAndersena2022",
-    imgFolderLength: 75,
-  },
-  {
-    id: "success3",
-    date: "28.08.2022",
-    title: "Występ na Pikniku Parafialnym",
-    content1: "",
-    signature: "",
-    imagePath: "images/wystepPiknik2022",
-    imgFolderLength: 24,
-  },
-  {
-    id: "success2",
-    date: "2022",
-    title:
-      "Poświęcenie pokarmów - Publiczne Przedszkole Parafialne w Mokrzyskach pw. Błogosławionej Karoliny Kózka",
-    content1: "",
-    signature: "",
-    imagePath: "images/wielkanoc2022",
-    imgFolderLength: 15,
-  },
-  {
-    id: "success1",
-    date: "2021",
-    title: "Pielgrzymowanie Przedszkolaków do św. Stanisława w Szczepanowie",
-    content1: "",
-    signature: "",
-    imagePath: "images/szczepanow2021",
-    imgFolderLength: 51,
-  },
+	{
+		id: 'success3',
+		date: '28.08.2022',
+		title: 'Występ na Pikniku Parafialnym',
+		content1: '',
+		signature: '',
+		imagePath: 'images/wystepPiknik2022',
+		imgFolderLength: 24,
+	},
+	{
+		id: 'success2',
+		date: '2022',
+		title: 'Poświęcenie pokarmów - Publiczne Przedszkole Parafialne w Mokrzyskach pw. Błogosławionej Karoliny Kózka',
+		content1: '',
+		signature: '',
+		imagePath: 'images/wielkanoc2022',
+		imgFolderLength: 15,
+	},
 ];
